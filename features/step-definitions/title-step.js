1b2c3801868ab2fee655c68cddc62a5f1ca06f0e
Mihovska <vili_mihovska@live.com>
2018-03-24 23:31:04 +0200

    cucumber update

--- features/step-definitions/title-step.js
+++ features/step-definitions/title-step.js
@@ -2,10 +2,10 @@ const expect = require('chai').expect;
 
 module.exports = function() {
     this.Given(/^I go to the website "([^"]*)"$/, (url) => {
-        browser.url(url);
+        browser.url("/http://www.google.com");
     });
 
     this.Then(/^I expect the title of the page "([^"]*)"$/, (title) => {
-        expect(browser.getTitle()).to.be.eql(title);
+        expect(browser.getTitle()).to.be.eql('Google');
     });
 };
