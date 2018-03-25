d14b1e1fe185ca3d00449f89d8e11979230ba586
Mihovska <vili_mihovska@live.com>
2018-03-25 11:53:20 +0300

    create step definitions

--- features/step-definitions/login.js
+++ features/step-definitions/login.js
@@ -1,17 +1,85 @@
-const expect = require('chai').expect;
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+chai.should();
+import LoginPage from '../pageObjects/LoginPage';
+import MyProfilePage from '../pageObjects/MyProfilePage';
+ 
+const loginPage = new LoginPage();
+const profilePage = new MyProfilePage();
+ 
+module.exports = function () {
+    this.Given('I am on the Google Calendar login page', () =>
+        loginPage.goToLoginPage()
+    );
+ 
+    this.When('I fill in "Email" with', () =>
+        loginPage.typeUsername('apptestautomation7@gmail.com')
+    );
 
-module.exports = function() {
-    Given('I go to the website {string}', function (string, callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
-      });
+    this.And('I press "Next"', () =>
+        loginPage.submit()
+    );
+    this.And('I fill in "Password" with', () =>
+        loginPage.typePassword('Nighthawk_f22')
+    );
 
-      Then('I expect the title of the page to be {string}', function (string, callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
-      });
+    this.And('I press "Next"', () =>
+        loginPage.submit()
+    );
+    
+    this.Then('I should be redirected to my profile page', () =>
+        profilePage.getProfilePageUrl().then(currentUrl => currentUrl.should.contain('https://calendar.google.com/calendar/r')
+    ));
 
-    this.Then(/^I expect the title of the page "([^"]*)"$/, (title) => {
-        expect(browser.getTitle()).to.be.eql('Google');
-    });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var {defineSupportCode} = require('cucumber');
+//const expect = require('chai').expect;
+
+
+defineSupportCode(function({ Given, Then, When }) {
+    Given('I am on the Google Calendar login page', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+
+      When('I fill in {string} with {string}', function (string, string2, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+
+      When('I press {string}', function (string, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+
+      When('I fill in {string} with {string}', function (string, string2, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+
+      When('I press {string}', function (string, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+
+      Then('I should be on my Calendar home page', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+    }); */
